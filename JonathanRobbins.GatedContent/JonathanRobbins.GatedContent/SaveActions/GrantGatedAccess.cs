1a4d2c171df9b76bedb6f99a7a496b3da1bc3e76
islaytitans <jonathan.m.robbins@gmail.com>
2016-06-30 13:00:47 +0100

    Dev / imeplementation of the binary gated access

---
+++ JonathanRobbins.GatedContent/JonathanRobbins.GatedContent/SaveActions/GrantGatedAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Sitecore.Data;
+using Sitecore.WFFM.Abstractions.Actions;
+using Sitecore.WFFM.Actions.Base;
+
+namespace JonathanRobbins.GatedContent.SaveActions
+{
+    public class GrantGatedAccess : WffmSaveAction
+    {
+        public override void Execute(ID formId, AdaptedResultList adaptedFields, ActionCallContext actionCallContext = null,
+            params object[] data)
+        {
+            HttpCookie cookie = new HttpCookie("Gated Access")
+            {
+                Value = "1",
+                Expires = DateTime.Today.AddYears(10),
+            };
+
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+    }
+}
